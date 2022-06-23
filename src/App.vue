64e234cd2b90552a20760c57f5a4f3a7baea570c
iasvobodin <iasvobodin@gmail.com>
2022-06-23 16:11:03 +0500

    need to fix build

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import { ref } from 'vue';
+import type { Ref } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
-
+const objectsURL = ref<Array<string>>([])
 Neutralino.events.on('ready', () => {
   Neutralino.os.showMessageBox('Welcome', 'Hello Neutralinojs');
 });
@@ -10,7 +12,23 @@ async function chooseFolder() {
   const entry = await Neutralino.os.showFolderDialog('Choose a folder');
   console.log('You have selected:', entry);
   const entries = await Neutralino.filesystem.readDirectory(entry);
-  console.log('Content: ', entries.filter(e => e.type === 'FILE'))
+  const imageEntr = entries.filter(e => e.type === 'FILE' && e.entry.endsWith('jpg'))
+  console.log('ImageContent:', imageEntr)
+
+  imageEntr.forEach(async (e) => {
+    let data = await Neutralino.filesystem.readBinaryFile(`${entry}/${e.entry}`);
+    const blobb = new Blob([data]);
+    objectsURL.value.push(URL.createObjectURL(blobb))
+
+  })
+
+  // let data = await Neutralino.filesystem.readBinaryFile(`${entry}/${imageEntr[0].entry}`);
+  // let view = new Uint8Array(data);
+  // const blobb = new Blob([data]);
+  // console.log('Binary content: ', view, blobb);
+  // objectURL.value = URL.createObjectURL(blobb);
+
+
 }
 async function readDirectory() {
   const entries = await Neutralino.filesystem.readDirectory('C:/Users/isvobodin/Desktop/n');
@@ -23,6 +41,7 @@ async function readDirectory() {
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite + Neutralino" /><br>
   <button @click="chooseFolder">chooseFolder</button><br>
   <button @click="readDirectory">readDirectory</button><br>
+  <img v-for="src in objectsURL" style="width: 200px; height: auto; " :src="src" alt="">
 </template>
 
 <style>
